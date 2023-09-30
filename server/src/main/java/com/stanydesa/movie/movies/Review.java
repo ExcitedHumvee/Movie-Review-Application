6d77ea65ed6cb20678249c6648f9a691c2a996db
ExcitedHumvee <35200248+ExcitedHumvee@users.noreply.github.com>
2023-09-30 23:42:25 +0300

    getters and setters

--- server/src/main/java/com/stanydesa/movie/movies/Review.java
+++ server/src/main/java/com/stanydesa/movie/movies/Review.java
@@ -17,6 +17,38 @@ public class Review {
     private LocalDateTime created;
     private LocalDateTime updated;
 
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
     public Review(String body, LocalDateTime created, LocalDateTime updated) {
         this.body = body;
         this.created = created;
